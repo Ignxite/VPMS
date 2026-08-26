039ad0b018b216433228cc565d054a5232c5721b
Ritesh Kumar <ritesh@vpms.dev>
2026-08-26 14:55:50 +0530

    Add: Global styles and TypeScript type definitions

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+import * as THREE from "three";
+import { Object3DNode } from "@react-three/fiber";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "three-line": Object3DNode<THREE.Line, typeof THREE.Line>;
+    }
+  }
+
+  interface Window {
+    riosExportModel?: (mode: "glb" | "fleet") => void;
+  }
+}
